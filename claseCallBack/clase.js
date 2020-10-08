8d7ac603106f30309690d54f07360534a99659bd
Daniela Garcia <dsgarcia@espol.edu.ec>
2020-10-08 12:51:27 -0500

    Callbacks

--- claseCallBack/clase.js
+++ claseCallBack/clase.js
@@ -2,14 +2,23 @@ const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
 const opts = { crossDomain: true}
 
-const onPeopleResponse = function (persona) {
-    console.log(`Hola yo soy, ${persona.name}`)
-}
 
-function obtenerPersonaje(id){
+function obtenerPersonaje(id, callback){
     const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
-    $.get(url, opts, onPeopleResponse)
+    $.get(url, opts, function (persona) {
+        console.log(`Hola yo soy, ${persona.name}`)
+
+        if (callback){
+            callback()
+        }
+    } )
     
 }
 
-obtenerPersonaje(1)
+obtenerPersonaje(1, function(){
+    obtenerPersonaje(2, function(){
+        obtenerPersonaje(3, function(){
+            obtenerPersonaje(4)
+        })
+    })
+})
